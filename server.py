1c24a8852cea2b90401b99c5f76be62ed2ae9050
Adrien Dumontet <65352453+LeLaboDuGame@users.noreply.github.com>
2025-02-04 14:45:42 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask("")
+
+@app.route("/")
+def home():
+    print("user connect to home")
+    return "Le plus beau des bot est en ligne ! genre vrmt le plus beau ! Regarde comment je suis beau et fort ! JE PING ET OUI JE PING DANS UN CHANNEL TRUC DE DINGUE !!!!!!!!"
+
+def run_server():
+    print("Im online baby !")
+    app.run(host='0.0.0.0', port=8080)
