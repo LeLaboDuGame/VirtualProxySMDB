99e4be17a6b4a03ec20527dd1331a02f3a421973
Adrien Dumontet <65352453+LeLaboDuGame@users.noreply.github.com>
2025-03-06 17:16:56 +0100

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,23 @@ from threading import Thread
 from flask import request
 from flask_socketio import SocketIO
 
+@app.route("/", methods=["POST", "GET"])
+def home():
+    print("user connect to home")
+    if request.method == "POST":
+        data = request.form
+        print(data)
+        print("post")
+        return "send !"
+
+    if request.method == "GET":
+        print("get")
+        return render_template("index.html")
+    
+
+    return "nothing to show"
+
+
 
 def run_server():
     print("Im starting...")
@@ -24,21 +41,5 @@ def handle_message(data):
 def handle_message(data):
     print('received message: con ' + data)
     
-@app.route("/", methods=["POST", "GET"])
-def home():
-    print("user connect to home")
-    if request.method == "POST":
-        data = request.form
-        print(data)
-        print("post")
-        return "send !"
-
-    if request.method == "GET":
-        print("get")
-        return render_template("index.html")
-    
-
-    return "nothing to show"
-
 
 
