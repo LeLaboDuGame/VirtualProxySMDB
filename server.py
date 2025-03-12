3f4ef1c391299396e59217bccdc8d4c80dcfc310
Adrien Dumontet <65352453+LeLaboDuGame@users.noreply.github.com>
2025-03-12 10:13:55 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from flask_socketio import SocketIO, send, emit
 
 app = Flask("")
 socketio = SocketIO(app)
-
+users = {}
 
 @socketio.on('sendmsg')
 def handle_message(data):
@@ -36,10 +36,26 @@ def home():
     return "nothing to show"
 
 
+@socketio.on('earn')
+def handle_message(data):
+    emit("updatepnt", {"pnt": users[data.user]}
+    lb = dict(sorted(users.items(), key=lambda item: item[1]))
+    emit("leaderboard", {"lb": str(lb).replace(",","\n")}, broadcast=True)
 
-def run_server():
+@app.route("/clicker", methods=["POST", "GET"])
+def home():
+    print("user connect to home")
+    if request.method == "GET":
+        print("get")
+        return render_template("clicker.html")
+    
+
+    return "nothing to show"
+
+
+        def run_server():
     print("Im starting...")
-    socketio.run(app, allow_unsafe_werkzeug=True, host="0.0.0.0", port=80)
+    socketio.run(app, allow_unsafe_werkzeug=True, host="0.0.0.0", port=80, debug=True)
     print("Im online baby !")
 
 
