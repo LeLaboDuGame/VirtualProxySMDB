50a065bc974d3c28ba91ed8141c0b637ffc34446
Adrien Dumontet <65352453+LeLaboDuGame@users.noreply.github.com>
2025-03-12 11:39:07 +0100

    Update server.py

--- server.py
+++ server.py
@@ -47,7 +47,7 @@ def handle_message(data):
     else:
         users[data["user"]][0] += users[data["user"]][1]
     emit("updatepnt", {"pnt": users[data["user"]][0]})
-    lb = [list(users.keys())[u] + ":" + users[list(users.keys())[u]][0] for u in range(len(dict(sorted(users.items(), key=lambda item: item[1]))))]
+    lb = [f"{list(users.keys())[u]} : {users[list(users.keys())[u]][0]} \n" for u in range(len(dict(sorted(users.items(), key=lambda item: item[1]))))]
     emit("leaderboard", {"lb": str(lb).replace(",","\n")}, broadcast=True)
     r+=1
     if r == 10:
